628fbfddca7acb6cea0c5acdcc65b258c7e614cc
PaintRock <6126@holbertonstudents.com>
2024-01-03 21:00:24 +0000

    nine

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 """Annotate a given funct and return the values with type"""
 from typing import List, Tuple
 
+
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """ return the values and appropriate type"""
     return[(i, len(i)) for i in lst]
