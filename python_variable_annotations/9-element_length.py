88f7aaf30071e29ca23c90a8017261eb820c839a
PaintRock <6126@holbertonstudents.com>
2024-01-03 21:18:58 +0000

    does nothing at exec still, and shortened the comment

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,5 +5,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]: 
-    """return the tuple and something else is what is req"""
+    """return the tuple and something else req"""
     return [(i, len(i)) for i in lst]
