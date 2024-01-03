d56ede4f13eae1825f455b07cd740c9928961532
PaintRock <6126@holbertonstudents.com>
2024-01-03 20:40:34 +0000

    sseven

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""function that takes a string, int, or float and returns a tuple"""
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns the first element of the tuple as the string
+    and the square root of the float"""
+    return (k, float(v ** 2))
