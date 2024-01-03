914481b31f47df917dc50c052a6ba9e1c68e2ed6
PaintRock <6126@holbertonstudents.com>
2024-01-03 05:41:31 +0000

    two

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Returns the floor"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor value of the input float.
+    """
+    return math.floor(n)
