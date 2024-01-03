1930c6b5c1e86efd1e60d2d1d0841a6fbdb91497
PaintRock <6126@holbertonstudents.com>
2024-01-03 18:51:17 +0000

    threev4

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-"""Convert a float to its string representation """
+    """Convert a float to its string representation"""
 
 
 def to_str(n: float) -> str:
-    """Args:
-        n (float): The input float.as_integer_ratio
-    
-    Returns: 
-        str: The string representattion of the input float"""
-return str(n)
+    """Convert a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
