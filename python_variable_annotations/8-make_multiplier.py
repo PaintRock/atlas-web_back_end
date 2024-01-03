12e86369f22cf7ad24e6e94b1267eb6e3ec641b5
PaintRock <6126@holbertonstudents.com>
2024-01-03 20:52:20 +0000

    eight

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""Function that takes a float and returns a multipied float"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a float adn returns a function"""
+    def multiplier_func(num: float) -> float:
+        """makes the func into the float"""
+        return num * multiplier
+    return multiplier_func
