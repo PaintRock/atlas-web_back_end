58a2c5ba7032309731c0540842962d38d48eab83
PaintRock <6126@holbertonstudents.com>
2024-01-03 19:13:20 +0000

    nothing

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-    """Convert a float to its string representation"""
+"""Convert a float to its string representation"""
 
 
 def to_str(n: float) -> str:
-    """Convert a float to its string representation.
+    """Converted a float to its string representation.
 
     Args:
         n (float): The input float.
