9d932dc420f119963d819a0a29a3bee52b69e672
PaintRock <6126@holbertonstudents.com>
2024-01-03 19:32:15 +0000

    4vmakingsense

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+"""Define and annotate variables
+'a' is a equal to 1
+'pi' is as it should be
+'i_understand_annotation' is funny, I do not even understan my computer
+'school' should be Atlas"""
+
+a: int = 1
+    
+pi: float = 3.14
+    
+i_understand_annotations: bool = True
+    
+school: str = "Holberton"
