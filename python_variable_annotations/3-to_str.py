9b0caeb08aaff4939c4342afa722bd4ccaaef4c7
PaintRock <6126@holbertonstudents.com>
2024-01-03 18:47:14 +0000

    threev1

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""Convert a float to its string representation """
+
+
+def to_str(n: float) -> str:
+    """Args:
+        n (float): The input float.as_integer_ratio
+    
+    Returns: 
+        str: The string representattion of the input float"""
+return str()
