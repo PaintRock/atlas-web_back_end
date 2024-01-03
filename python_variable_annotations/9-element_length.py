441c7be001c3de647f8cf521b72a7fe23a010c84
PaintRock <6126@holbertonstudents.com>
2024-01-03 21:20:38 +0000

    he is a jerk

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-"""Annotate a given funct and return the values with type"""
+"""Annotate a funct and return the values with type"""
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]: 
-    """return the tuple and something else req"""
+    """return the tuple with value type"""
     return [(i, len(i)) for i in lst]
