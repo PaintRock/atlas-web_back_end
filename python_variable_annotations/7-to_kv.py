47af38f860aa72aec82d107cbc45694f01e44f8a
PaintRock <6126@holbertonstudents.com>
2024-01-03 20:43:08 +0000

    lines

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-"""function that takes a string and returns a tuple"""
+"""function that takes a string, int, or float and returns a tuple"""
 from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns the first element of the tuple as the string
