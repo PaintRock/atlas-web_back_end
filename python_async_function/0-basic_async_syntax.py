ca1c9ab30485eab1fb61bff93f5610f5bbd4b018
PaintRock <6126@holbertonstudents.com>
2024-01-04 05:08:52 +0000

    zero V 7

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10 -> float):
+async def wait_random(max_delay: int = 10)-> float:
     """Max delay set at value of 10"""
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
