6c691bf7e8aa3fb260e1aaa45a71f042c49a17c5
PaintRock <6126@holbertonstudents.com>
2024-01-04 04:44:09 +0000

    zero V 3

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,3 +10,8 @@ async def wait_random(max_delay=10):
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
+# Test cases
+print(asyncio.run(wait_random()))
+print(asyncio.run(wait_random(5)))
+print(asyncio.run(wait_random(15)))
