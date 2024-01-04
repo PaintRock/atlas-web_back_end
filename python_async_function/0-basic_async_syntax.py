a115efec4a33bfe37720eabeb1ecb3227f42acbb
PaintRock <6126@holbertonstudents.com>
2024-01-04 05:10:09 +0000

    zero V 7

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-""" Asynchronous coroutine that accepts two"""
+""" Asynchronous coroutine"""
 import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10)-> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Max delay set at value of 10"""
-    delay: float = random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
