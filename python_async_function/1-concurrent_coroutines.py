9b893283047e72c89702c15c8ba41be15aa57fc2
PaintRock <6126@holbertonstudents.com>
2024-01-04 05:00:52 +0000

    one V one

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
-""""""
+"""Does some stuff that I dont """
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """This is the wait period"""
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """" and this is something else"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
+
+# Test cases
+print(asyncio.run(wait_n(5, 5)))
+print(asyncio.run(wait_n(10, 7)))
+print(asyncio.run(wait_n(10, 0)))
