d7658e773e4e7bf33cef6439e2d99afffb677f79
PaintRock <6126@holbertonstudents.com>
2024-01-05 19:26:10 +0000

    not right

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """This is the addition that I made"""
+    """This spawns wait_random """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-""""""
+""" This measures the runtime"""
+import time
+from asyncio import run
+from typing import List
+wait_n = __import__('1-concurrent_corotines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    start_time = time.time()
+    run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
