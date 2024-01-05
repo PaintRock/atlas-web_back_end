a3a0432f1c23f7d55fcf278bb3fc577da96a91d3
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:46:26 +0000

    Nope. now 1/6

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator():
     """Max delay set at value of 10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,)
+        yield random.uniform(0, 10)
