fde1a23638b8b3c52fdf91d7251f93f8f01e25c7
PaintRock <6126@holbertonstudents.com>
2024-01-05 21:43:34 +0000

    three V 2

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-"""A function (normal) that takes integer and returns asyncio.Task"""
+"""A function (normal) that takes an 
+integer and returns asyncio.Task"""
 import time
 import asyncio
 
 
 def task_wait_random(max_delay:int) -> asyncio.Task:
- """"Max delay is an int and returns asyncio.Task"""    
+ """"Max delay is an int and 
+ returns asyncio.Task"""
+     async def wrapper():
+        return await wait_random(max_delay)
+
+    return asyncio.create_task(wrapper())
+    
