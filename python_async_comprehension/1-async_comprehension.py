1b60dd10e64fbbbd6d8184b57a16398898c59671
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:50:58 +0000

    two V 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-""" """
+"""Asynchronous generator"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """10 loops and then 10 rand"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
