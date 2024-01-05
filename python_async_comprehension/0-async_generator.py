7d16356f794bbde7eeeb3cb067bb84594828213b
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:39:13 +0000

    should be correct

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,4 +7,6 @@ import asyncio
 
 async def async_generator():
     """Max delay set at value of 10"""
-    return [i async for i in range(10)]
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
