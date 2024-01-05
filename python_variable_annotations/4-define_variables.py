7c7b59fa352770e2c23d6b4f8fa1c95c59c06911
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:06:01 +0000

    one pt

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """Define and annotate"""
 
 
-a: int = 1    
-pi: float = 3.14    
-i_understand_annotations: bool = True    
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
 school: str = "Holberton"
