301b423d353498e51235b5904e7c4594958bb9e0
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:55:33 +0000

    two V 3

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,7 @@ import asyncio
 async_generator = __import__('0-async_generator.py').async_generator
 
 
-async def async_generator():
+async def async_comprehension():
     """10 loops and then 10 rand"""
     for _ in range(10):
         await asyncio.sleep(1)
