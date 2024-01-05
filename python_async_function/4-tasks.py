98455f9b45c709e6f947cf160874dbc8b3f36342
PaintRock <6126@holbertonstudents.com>
2024-01-05 22:24:58 +0000

    4 V not even trying

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-"""POS"""
+""" Asynchronous coroutine"""
+import asyncio
+from random import uniform
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """This spawns wait_random """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
