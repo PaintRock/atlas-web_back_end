e56aab28bf35c181a17952d07da7ab2ced3c1939
PaintRock <6126@holbertonstudents.com>
2024-01-05 21:35:05 +0000

    three V 1

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
-""""""
+"""A function (normal) that takes integer and returns asyncio.Task"""
+import time
+import asyncio
+
+
+def task_wait_random(max_delay:int) -> asyncio.Task:
+    
