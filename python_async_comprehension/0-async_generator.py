013d011b9bb66fe97f0a5f822002048d5bdaaa66
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:48:50 +0000

    Back to 5/6.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-"""Asynchronous coroutine"""
+"""Asynchronous generator"""
 import random
 import asyncio
 
 
 async def async_generator():
-    """Max delay set at value of 10"""
+    """10 loops and then 10 rand"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
