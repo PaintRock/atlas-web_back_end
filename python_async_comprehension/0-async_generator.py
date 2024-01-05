66761d074dead54f24f5117562650b3ee26fd270
PaintRock <6126@holbertonstudents.com>
2024-01-05 23:27:30 +0000

    anything

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
+""" Asynchronous coroutine"""
+import random
+import asyncio
 
+
+async def async_generator():
+    """Max delay set at value of 10"""
+    return [i async for i in range(10)]
