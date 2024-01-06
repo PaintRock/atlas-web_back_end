aa132651de81886f719ab859f4b3c5d7a000124a
PaintRock <6126@holbertonstudents.com>
2024-01-06 04:38:38 +0000

    two V 10

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-"""Asynchronous generator"""
+"""Asynchronous comprehensing"""
 import asyncio
 import random
 from 0-async_generator import async_generator
 
 
 async def async_comprehension():
-    """This is wrong becaause I have no await"""
+    """Async comprehensing over async generator"""
      async_comprehension = [await async_generator() for _ in range(10)]
     return async_comprehension
