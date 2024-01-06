3bba710c7828518353057abcf273d3bcc8f9eb9c
PaintRock <6126@holbertonstudents.com>
2024-01-06 04:32:50 +0000

    two V 8

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,8 +10,5 @@ async_generator = __import__('0-async_generator.py').async_generator
 
 async def async_comprehension():
     """This is wrong becaause I have no await"""
-    async_comprehension = []
-    for _ in range(10):
-        number = async_generator()
-        async_comprehension.append(number)
+     async_comprehension = [await async_generator() for _ in range(10)]
     return async_comprehension
