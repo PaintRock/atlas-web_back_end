303cc24aab5f6749cfec0a7fc85e5ba2429d4c02
PaintRock <6126@holbertonstudents.com>
2024-01-06 05:06:59 +0000

    mean

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Asynchronous generator"""
-
+import typing
 import random
 import asyncio
 
 
-async def async_generator() ->typing.Generator[float, None, None]:
+async def async_generator() -> typing.Generator[float, None, None]:
     """10 loops and then 10 rand"""
     for _ in range(10):
         await asyncio.sleep(1)
