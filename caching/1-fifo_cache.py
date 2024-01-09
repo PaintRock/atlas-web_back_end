e08ce1d6e638e023fb2e2b8b0551b9e10feeae5e
PaintRock <6126@holbertonstudents.com>
2024-01-09 06:47:11 +0000

    1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -13,23 +13,26 @@ class FIFOCache(BaseCaching):
         """Calling parent init
         """
         super().__init__()
+        self.queue = []
 
     def put(self, key, item):
         """ assign the key to item"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= self.MAX_ITEMS:
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             """FIFO: Remove the first item added to the cache"""
-            discarded_key, _ = next(iter(self.cache_data.items()))
+            discarded_key = self.queue.pop(0)
             del self.cache_data[discarded_key]
             print(f"DISCARD: {discarded_key}\n")
 
         """ Add or update the item in the cache"""
         self.cache_data[key] = item
+        self.queue.append(key)
 
     def get(self, key):
         """If key is none return none """
         if key is None or key not in self.cache_data:
             return None
+
         return self.cache_data[key]
