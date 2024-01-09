ed2d6af4cde71a32fefc4509fd78b279897bce3c
PaintRock <6126@holbertonstudents.com>
2024-01-09 21:29:13 +0000

    3v40

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -45,3 +45,14 @@ class Server:
             'data': page_data,
             'page_size': page_size,
         }
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
