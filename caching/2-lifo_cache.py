471b967f46fe7587a90cc7aadcf98637e9376082
PaintRock <6126@holbertonstudents.com>
2024-01-09 17:56:11 +0000

    2 might work V1

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -2,29 +2,38 @@
 """Lifo caching"""
 
 from BaseCaching import BaseCaching
-BaseCaching = __import__('base_caching').BaseCaching
+"""BaseCaching = __import__('base_caching').BaseCaching"""
 
 
 class LIFOCache(BaseCaching):
     """created class LIFO"""
+
     def __init__(self):
         """Calling parent"""
         super().__init__()
+        self.queue = []
 
     def put(self, key, item):
         """Set the key"""
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            """ LIFO: Remove the last item added to the cache"""
-            last_key = list(self.cache_data.keys())[-1]
-            del self.cache_data[last_key]
-            print(f"DISCARD: {last_key}\n")
+        if key in self.cache_data:
+            """Update the value for an existing key"""
+            self.cache_data[key] = item
+            """Remove the existing key from the queue"""
+            self.queue.remove(key)
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                """ LIFO: Remove the last item added to the cache"""
+                last_key = list(self.cache_data.keys())[-1]
+                del self.cache_data[last_key]
+                print(f"DISCARD: {last_key}")
 
         """ Add or update the item in the cache """
-        .6355
         self.cache_data[key] = item
+        """Append the new key to the end of the queue"""
+        self.queue.append(key)
 
     def get(self, key):
         """ return the cache"""
