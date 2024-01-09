4c975d88a39a763353854109b47cbfa3de8c6dc1
PaintRock <6126@holbertonstudents.com>
2024-01-09 21:26:44 +0000

    3v40

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -28,16 +28,20 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
-        if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
-        return self.__indexed_dataset
-
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        pass
+        dataset = self.indexed_dataset()
+
+        if index is None:
+            return {'index': None, 'data': [], 'page_size': page_size}
+
+        if index >= len(dataset):
+            return {'index': None, 'data': [], 'page_size': page_size}
+
+        next_index = min(index + page_size, len(dataset))
+        page_data = [dataset[i] for i in range(index, next_index)]
+
+        return {
+            'index': index,
+            'data': page_data,
+            'page_size': page_size,
+        }
