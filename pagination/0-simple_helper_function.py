95d9bdd323f38cc0d860e767d29c44671b1ff93d
PaintRock <6126@holbertonstudents.com>
2024-01-09 05:29:26 +0000

    one

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Simple function"""
+
+def index_range(page, page_size):
+    """create a tuple"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
