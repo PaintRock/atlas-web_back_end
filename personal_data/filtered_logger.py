6477d4f113eee9c4334771af18e43fb0671137e3
PaintRock <6126@holbertonstudents.com>
2024-01-15 22:57:50 +0000

    lambda

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -21,4 +21,10 @@ def filter_datum(fields, redaction, message, separator):
         """
 
     return re.sub(fr'(?<={separator}|^)({"|".join(fields)})=[^;]*', lambda match: f'{match.group(1)}={redaction}', message)
-      
+     
+     fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+
+for message in messages:
+    print(filter_datum(fields, 'xxx', message, ';'))
+     
