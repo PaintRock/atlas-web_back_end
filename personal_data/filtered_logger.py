a489f5bb152d38cb07fdb632e82da59952f30ef6
PaintRock <6126@holbertonstudents.com>
2024-01-15 23:33:17 +0000

    lambda

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@ import re
 """A function that returns
 the log message obfuscated"""
 
+
 def filter_datum(fields, redaction, message, separator):
     """ Args
         fields: a list of strings representing all
@@ -10,21 +11,23 @@ def filter_datum(fields, redaction, message, separator):
         
         redaction: a string representing by what
         the field will be obfuscated
-        
+
         message: a string representing the log line
-        
-        separator: a string representing by which 
-        character is separating all fields in the 
+
+        separator: a string representing by which
+        character is separating all fields in the
         log line (message)
-        
+
         Returns: "xxx"ed out stuff in certain fields
         """
 
     return re.sub(fr'(?<={separator}|^)({"|".join(fields)})=[^;]*', lambda match: f'{match.group(1)}={redaction}', message)
-     
-     fields = ["password", "date_of_birth"]
+
+    fields = ["password", "date_of_birth"]
+
+
 messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
 
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
-     
+    
