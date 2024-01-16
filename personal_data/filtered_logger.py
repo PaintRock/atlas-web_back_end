032d52d6d9c6115794a4f1b8eaa0baff268fa1f2
PaintRock <6126@holbertonstudents.com>
2024-01-16 21:12:03 +0000

    wtfgit add .

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
+"""Write a function called filter_datum
+that returns the log message obfuscated"""
 import re
-from typing import List
-"""A function that returns
-the log message obfuscated"""
+import typing
+import logging
 
 
-def filter_datum(fields: typing.List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ Args
-        fields: a list of strings representing all
-        fields to obfuscate
-
-        redaction: a string representing by what
-        the field will be obfuscated
-
-        message: a string representing the log line
-
-        separator: a string representing by which
-        character is separating all fields in the
-        log line (message)
-
-        Returns: "xxx"ed out stuff in certain fields
-        """
-
+def filter_datum(fields: typing.List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """The function should use a regex to replace
+    occurrences of certain field values"""
     for field in fields:
         pattern = r'({})([^{}]+)'.format(field + '=', separator)
         message = re.sub(pattern, r"\1{}".format(redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: typing.List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
