33858f21ad5ca450853c1a3119a02f8799225384
PaintRock <6126@holbertonstudents.com>
2024-01-18 04:57:06 +0000

    addressed

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,6 +14,20 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -21,18 +35,26 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
+@app.before_request
+def before_request() -> str:
+    """ Before Request Handler
+    Requests Validation
     """
-    return jsonify({"error": "Forbidden"}), 403
+    if auth is None:
+        return
 
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
