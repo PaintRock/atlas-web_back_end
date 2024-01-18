f0d008a9cbe746ccdc4b7557435123e9845d843e
PaintRock <6126@holbertonstudents.com>
2024-01-18 04:52:32 +0000

    lines

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -35,21 +35,6 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.before_request
-def before_request():
-    """pre-request handler"""
-    if auth is None:
-        return
-    if not auth.require_auth(request.path, ['/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
