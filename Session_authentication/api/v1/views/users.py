c870e89beba65c61d11a26ec41b9b37e562d1e68
PaintRock <6126@holbertonstudents.com>
2024-01-20 22:39:42 +0000

    zero

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -27,19 +27,9 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-
-    if user_id == 'me' and request.current_user is None:
-        """added for zero"""
-        abort(404)
-
     user = User.get(user_id)
     if user is None:
         abort(404)
-
-    if user_id == 'me' and request.current_user is not None:
-        """added for zero"""
-        return jsonify(request.currnet_user.to_json()
-
     return jsonify(user.to_json())
 
 
