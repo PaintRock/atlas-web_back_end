9204773c018b968ff0dfd969fcf81753551154e4
PaintRock <6126@holbertonstudents.com>
2024-01-20 03:45:36 +0000

    me

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,10 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """empty class inheriting"""
     pass
+def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    not isinstance authorization_header is None or
+    if (authorization_header, str):
+        return None
+    if not authorization_header.startswith("Basic "):
+        return None
+    return authorization_header[len("Basic "):]
---
+++ Session_authentication/Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """this is the class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required for the given path."""
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        """Ensure slash tolerance by handling paths with and
+        without trailing slashes"""
+        normalized_path = path.rstrip('/') + '/'
+
+        return normalized_path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """Get the authorization header from the request."""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current user from the request."""
+        return None
