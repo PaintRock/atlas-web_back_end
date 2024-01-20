dd1a0631a950ff8d4566b49f9635ba26dca54579
PaintRock <6126@holbertonstudents.com>
2024-01-20 23:40:38 +0000

    me

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -7,5 +7,12 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """SessionAuth class"""
-    pass
+    """create a CLASS ATTRIBUTE initialize by
+    an empty dictionary"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create an instance method that creates a
+        Session ID for a user_ID (HUH?)"""
+        if user_id is None or not isinstance (user_id, str):
+            return None
