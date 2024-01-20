a097280f3fda0d7177c512722cc7e09069d1c192
PaintRock <6126@holbertonstudents.com>
2024-01-20 23:24:37 +0000

    one

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -23,6 +24,14 @@ if AUTH_TYPE == "auth":
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    pass
 
 
 @app.errorhandler(401)
