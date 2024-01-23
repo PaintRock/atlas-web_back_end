8148fc9bf1058d8779e21bcd5fc7423cdba3d093
PaintRock <6126@holbertonstudents.com>
2024-01-23 16:56:08 +0000

    zeroV2 indentation error

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
  
  
- class User(Base):
+class User(Base):
      __tablename__ = 'users'
 
      id = Column(Integer, primary_key=True)
