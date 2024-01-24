9c1ae77ab3f6db301247923a38f637b0d9935a60
PaintRock <6126@holbertonstudents.com>
2024-01-24 18:08:31 +0000

    twoV1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import InvalidRequestError
 from user import User
 
 from user import Base
@@ -37,3 +39,33 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find user by keyword arguments"""
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user           
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates users"""
+        user = self.find_user_by(id=user_id)
+        column_names = User.__table__.coulumns.keys()
+        for key in kwargs.keys():
+            if key not in column_name:
+                raise ValueError
+
+        for key, value in kwargs.item():
+            setattr(user, key, value)
+
+        self._session.commit()
