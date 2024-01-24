f2587e29732b0390fb6fd9fe2a5c52f9a7070208
PaintRock <6126@holbertonstudents.com>
2024-01-24 21:10:46 +0000

    all bad --- oops. trying to find the error on auth

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -43,7 +43,7 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid() -> str:
+    def _generate_uuid(self) -> str:
         """Generate a new UUID
         return New uuid"""
         new_uuid = uuid.uuid4()
