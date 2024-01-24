9202629c9ec43ac58c1d46081c8ce728eaf24b4f
PaintRock <6126@holbertonstudents.com>
2024-01-24 20:09:20 +0000

    try

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -18,5 +18,17 @@ def welcome():
     return jsonify(message)
 
 
+@app.route("/users", methods=["POST"])
+def register_users():
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError as e:
+        return jsonify({"message": str()}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
