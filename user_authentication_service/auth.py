eaec9da39d0ffda7364298459ababdfd2349ab16
PaintRock <6126@holbertonstudents.com>
2024-01-24 20:27:14 +0000

    eightV2 line too long

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -31,11 +31,12 @@ class Auth:
         else:
             raise ValueError(f'User {email} already exists')
 
-    def valid_login(self, email:str, password:str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """Checks validity of password"""
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
 
