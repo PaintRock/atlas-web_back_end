81062d8ca798c9018398fbe33a01e7199faf89f4
PaintRock <6126@holbertonstudents.com>
2024-01-24 18:36:19 +0000

    fourV1

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Authentication Module"""
+from db import DB
+from user import User
+from bcrypt import hashpw, gensalt, checkpw
+
+
+def _hash_password(password: str) -> bytes:
+    """Takes a pasword and returns bytes"""
+    return hashpw(password.encode('utf-8'), gensalt())
