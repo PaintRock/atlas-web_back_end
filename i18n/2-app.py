d0167b002e5a5cf156b6ec63afa04e44c171b1fb
PaintRock <6126@holbertonstudents.com>
2024-01-28 05:44:40 +0000

    two

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -18,11 +18,11 @@ class Config():
 @babel.localeselector
 def get_locale():
     """get locale function"""
-    return.request.accept_languages.best_match(['en', 'fr'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 app.config.from_object(Config)
-"""Use that class as config for Flask app"""
+"""not sure why this makes this work"""
 
 
 @app.route('/')
