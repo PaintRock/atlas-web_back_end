c961b29f843124d641a873baf106e9dd64234ad6
PaintRock <6126@holbertonstudents.com>
2024-01-28 05:09:20 +0000

    one

---
+++ i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Babel object in da house"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """Config class""" 
+    LANGUAGES = "en", "fr"
+    BABLE_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZON = 'UTC'
+
+
+app.config.from_object(Config)
+"""Use that class as config for Flask app"""
+
+
+@app.route('/')
+def root():
+    """basic Flask"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
