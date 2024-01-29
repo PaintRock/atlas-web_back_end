f8c9f2c80f22378268a137d8ad658fff835f1bea
PaintRock <6126@holbertonstudents.com>
2024-01-29 09:36:58 +0000

    zerofinal

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Test SUITE Unittest module Task """
-
+""" Unittest module Task """
 from unittest import TestCase, mock
 from unittest.mock import patch, Mock
 from parameterized import parameterized
@@ -15,7 +14,7 @@ class TestAccessNestedMap(TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ Test method return output """
+        """ Test method outputs """
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
 
