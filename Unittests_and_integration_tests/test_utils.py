4463f5779cfe80c4f09a870143c25286247b8ff6
PaintRock <6126@holbertonstudents.com>
2024-01-29 09:34:38 +0000

    pycode

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-""" this is some doc"""
+""" Test SUITE Unittest module Task """
+
 from unittest import TestCase, mock
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """This is the class for the first"""
+class TestAccessNestedMap(TestCase):
+    """ Class for testing Nested Map function """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), ("b": 2)),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
         """ Test method return output """
