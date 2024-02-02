b36d8310c0c8199fb925f3c3512fb9eb4600a8cb
PaintRock <6126@holbertonstudents.com>
2024-02-02 19:33:33 +0000

    zero

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -10,7 +10,7 @@ class Cache:
     
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis = redis.flushdb()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uudi4())
