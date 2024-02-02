4b1b6ac4d35bfb9b6f21994719ad8fc83e8bc66c
PaintRock <6126@holbertonstudents.com>
2024-02-02 19:30:48 +0000

    zerotypo

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,11 +7,12 @@ from typing import Union
 
 class Cache:
     """Create a class Cache"""
+    
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis = redis.flusdb()
-        
-    def store(data: Union[str, bytes, int, float])->str:
+        self._redis = redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uudi4())
         self._redis.set(key, data)
         return key
