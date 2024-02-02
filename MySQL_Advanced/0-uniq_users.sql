fc1c9bd186656314cadcd0fab331fb841739d5f9
PaintRock <6126@holbertonstudents.com>
2024-02-02 21:06:54 +0000

    0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create a table that can be used on any database
-CREATE TABLE IF NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    'email' VARCHAR(255) NOT NULL UNIQUE, 
-    'name' VARCHAR(255)
+CREATE TABLE If NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
 );
