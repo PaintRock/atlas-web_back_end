7f0bd499e98d3d5471ec1a6abaca1e2ad477e539
PaintRock <6126@holbertonstudents.com>
2024-02-02 21:11:45 +0000

    ONE-BACKTICKS

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create a table that c
-CREATE TABLE If NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    'email' VARCHAR(255) NOT NULL UNIQUE, 
-    'name' VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    `email` VARCHAR(255) NOT NULL UNIQUE, 
+    `name` VARCHAR(255)
 );
