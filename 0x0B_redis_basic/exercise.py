07878c355b72919f42a7caaa33d6bf5499ade25a
PaintRock <6126@holbertonstudents.com>
2024-02-02 20:16:14 +0000

    one

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,12 +2,13 @@
 """This is a Redis module """
 import redis
 from uuid import uuid4, UUID
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
+
 
 class Cache:
     """Create a class Cache"""
-    
+
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -16,3 +17,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Convert data back to the desired format"""
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """"parameterize Cache with..."""
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> str:
+        """...the correct conversion function"""
+        return self.get(key, fn=int)
