5cbd20a85d3b24f8178c056ca94df3cb578001e5
PaintRock <6126@holbertonstudents.com>
2024-02-03 23:57:01 +0000

    4 order_decrease

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,12 +1,9 @@
 -- Create a trigger to decrease item quantity after adding a new order
-CREATE TRIGGER after_insert_order
+CREATE TRIGGER order_decrease
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     -- Decrease the quantity of the ordered item in the items table
     UPDATE items
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE item_id = NEW.item_id;
-END;
-//
-DELIMITER ;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
