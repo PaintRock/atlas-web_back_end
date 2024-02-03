7ab8deccba3c74e397b15b987e16108af6991b02
PaintRock <6126@holbertonstudents.com>
2024-02-03 04:32:19 +0000

    threeV2

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Glam rock as main style by longevity
-SELECT band_name 
+SELECT band_name, split - formed AS lifespan
 FROM metal_bands
 WHERE style = Glam rock
-ORDER BY (split - formed) AS lifespan DESC
+ORDER BY lifespan DESC
 ;
