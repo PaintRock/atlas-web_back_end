d4a80df819bf52a82cac6fae57563e5ad9ac08b4
PaintRock <6126@holbertonstudents.com>
2024-02-03 05:18:42 +0000

    four

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- Create a trigger to decrease item quantity after adding a new order
+DELIMITER //
+CREATE TRIGGER after_insert_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    -- Decrease the quantity of the ordered item in the items table
+    UPDATE items
+    SET quantity = quantity - NEW.quantity_ordered
+    WHERE item_id = NEW.item_id;
+END;
+//
+DELIMITER ;
