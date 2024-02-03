b168d278ced6da452be2b9a89fe4360e3cadb179
PaintRock <6126@holbertonstudents.com>
2024-02-03 23:59:41 +0000

    before insert on

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- Create a trigger to decrease item quantity after adding a new order
 CREATE TRIGGER order_decrease
-AFTER INSERT ON orders
+BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
     -- Decrease the quantity of the ordered item in the items table
