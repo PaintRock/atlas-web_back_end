106d8be26ee2dcef1a6cb7866bdf5fe6d1fc283b
PaintRock <6126@holbertonstudents.com>
2024-02-04 00:04:53 +0000

    bEGIN END

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -2,7 +2,9 @@
 CREATE TRIGGER order_decrease
 BEFORE INSERT ON orders
 FOR EACH ROW
-    -- Decrease the quantity of the ordered item in the items table
-UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;
+-- Decrease the quantity of the ordered item in the items table
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;    
