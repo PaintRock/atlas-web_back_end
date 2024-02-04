927e7b5fb63d2af8166828c8d448f9d16d5bfb50
PaintRock <6126@holbertonstudents.com>
2024-02-04 00:22:24 +0000

    cp

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,10 @@
 -- trigger to reset the valid_email when email has changed
-DELIMETER |
-CREATE TRIGGER update_email_bool
-BEFORE UPDATE ON users
+DELIMITER |
+CREATE TRIGGER email_bool BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
+    SET NEW.valid_email = 0;
     END IF;
 END;
 |
