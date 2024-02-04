867b47847b94312f6cf3eae840165a9924b96ab4
PaintRock <6126@holbertonstudents.com>
2024-02-04 20:57:31 +0000

    eight

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin env python 3
+//function that inserts a new document in a collection
+
+def list_all(mongo_collection):
+    """the pymongo collection object"""
+    documents = mongo_collection.find()
+    document_list = list(documents)
+    return document_list
