74021a07158f9a9a5cedfcba2791c81d568cfde6
PaintRock <6126@holbertonstudents.com>
2024-02-04 21:11:10 +0000

    nineV2

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """function that inserts a new document in a collection"""
+from unittest import result
+
 
 def list_all(mongo_collection, **kwargs):
     """the pymongo collection object"""
     documents = mongo_collection.insert_one(kwargs)
-    id = results.inserted_id
+    id = result.inserted_id
     return id
