2dc07f38097839dc909f17b526f2c2d5fb40285b
PaintRock <ann.knode@atlasschool.com>
2024-02-07 22:10:15 +0000

    elevenV1

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Document find list """
-    return mongo.collection.find({"topics": topic})
+    return mongo_collection.find({"topics": topic})
