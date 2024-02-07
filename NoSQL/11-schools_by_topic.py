2feb4bdc280de810aa1a3a1d61b3d267d679f1a2
PaintRock <ann.knode@atlasschool.com>
2024-02-07 22:35:29 +0000

    twelveV3

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,31 +1,7 @@
-from pymongo import MongoClient
+#!/usr/bin/env python3
+"""function that provides stats about Nginx"""
 
-def log_stats():
-    # Connect to MongoDB
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['logs']
-    collection = db['nginx']
 
-    # Get total count of logs
-    total_logs = collection.count_documents({})
-
-    # Count documents for each HTTP method
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    method_counts = {method: collection.count_documents({'method': method}) for method in methods}
-
-    # Count documents with method=GET and path=/status
-    status_check_count = collection.count_documents({'method': 'GET', 'path': '/status'})
-
-    # Display stats
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"    method {method}: {count}")
-    print(f"{status_check_count} status check")
-
-    # Close MongoDB connection
-    client.close()
-
-if __name__ == "__main__":
-    log_stats()
-    
+def schools_by_topic(logs, topic):
+    """Document find list """
+    return mongo_collection.find({"topics": topic})
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,7 +1,31 @@
-#!/usr/bin/env python3
-"""function that returns a list of schools"""
+from pymongo import MongoClient
 
+def log_stats():
+    # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
 
-def schools_by_topic(mongo_collection, topic):
-    """Document find list """
-    return mongo_collection.find({"topics": topic})
+    # Get total count of logs
+    total_logs = collection.count_documents({})
+
+    # Count documents for each HTTP method
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    method_counts = {method: collection.count_documents({'method': method}) for method in methods}
+
+    # Count documents with method=GET and path=/status
+    status_check_count = collection.count_documents({'method': 'GET', 'path': '/status'})
+
+    # Display stats
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"    method {method}: {count}")
+    print(f"{status_check_count} status check")
+
+    # Close MongoDB connection
+    client.close()
+
+if __name__ == "__main__":
+    log_stats()
+
