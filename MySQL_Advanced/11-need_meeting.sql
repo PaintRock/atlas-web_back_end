1b98cefe99a72c0ff6ca7f25d44e7db8e0a72d0d
PaintRock <ann.knode@atlasschool.com>
2024-02-07 19:45:23 +0000

    elevenV5

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,8 @@
 -- script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 DELIMITER //
-CREATE VIEW need_meeting AS
+CREATE VIEW need_meeting; AS
 SELECT name FROM students
 WHERE score < 80 ANd last_meeting is NULL or 
-last_meeting last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
+last_meeting DATEDIFF(CURRENT_DATE(), last_meeting) > 30);
 END;
 //
