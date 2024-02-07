c8879f53c0f6dab8de7ea881030f3da42a9073dd
PaintRock <ann.knode@atlasschool.com>
2024-02-07 22:26:09 +0000

    twelve

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
-""" Write a Python script that provides some stats about
-    Nginx logs stored in MongoDB
-"""
+""" Script to provide stats about Nginx logs stored in MongoDB """
+
 from pymongo import MongoClient
 
+def count_documents_with_method(collection, method):
+    """ Count the number of documents with a specific method """
+    return collection.count_documents({"method": method})
+
+def count_documents_with_status_path(collection, method, path):
+    """ Count the number of documents with a specific method and path """
+    return collection.count_documents({"method": method, "path": path})
+
 if __name__ == "__main__":
-    """ Database: logs
-        Collection: nginx
-    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    db = client.logs
+    collection = db.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        count = count_documents_with_method(collection, method)
+        print(f"\t{count} {method}")
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-        )
-    print(f'{status_check} status check')
+    status_path_count = count_documents_with_status_path(collection, "GET", "/status")
+    print(f"{status_path_count} logs where method=GET path=/status")
     
