39e2be1ec588604e52f9ac1dbd81cd346d49de26
PaintRock <ann.knode@atlasschool.com>
2024-02-07 22:14:40 +0000

    twelve

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
-"""function that returns a list of schools"""
+""" Write a Python script that provides some stats about
+    Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
 
+if __name__ == "__main__":
+    """ Database: logs
+        Collection: nginx
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-def schools_by_topic(mongo_collection, topic):
-    """Document find list """
-    return mongo_collection.find({"topics": topic})
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f'{status_check} status check')
+    
