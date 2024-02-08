d1c43ca6f6a83b31a7071cbb1894395e57417d5e
PaintRock <ann.knode@atlasschool.com>
2024-02-08 20:11:21 +0000

    one

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create a table 
+CREATE TABLE IF NOT EXIST users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255),
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
