c2c52e1be4656365460a8494699c1d7649d59c18
PaintRock <ann.knode@atlasschool.com>
2024-02-08 15:43:29 +0000

    gitignore

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90041e4aeae09a9500788ff18685fc40fe41bb1cd1909422f1bf21314b4c2439
-size 184
+oid sha256:c858122522b84350b0d371a4a25b0dd2941df73ba1256f291892341a0d9e3cbd
+size 199
--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a8ef3cdc3e6ffb0d7fee91aec6c639e53d5b6fc2c3cc1ea3eb0f929fa5c03f1
-size 94
+oid sha256:6634c2be21adb47d70c8bf6180efaf9bf542db6a4f741d86bce3933837774c0f
+size 95
