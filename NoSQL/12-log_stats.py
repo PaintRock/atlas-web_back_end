f4ac5adb026ea51d9be9dee56cae4b235132236d
PaintRock <ann.knode@atlasschool.com>
2024-02-08 00:24:53 +0000

    me2

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,20 +3,5 @@
 from pymongo import MongoClient
 from dump import logs
 
-
-def log_stats():
-    """Provides some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient()
-    db = dump.logs
-    logs = nginx.bson
-    print("{} logs".format(logs.count_documents({})))
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        print(f"\tmethod {method}: " +
-              f"{logs.count_documents({'method': method})}")
-    print(f"{logs.count_documents({'method': 'GET', 'path': '/status'})} \
-status check")
-
 if __name__ == "__main__":
     log_stats()
