90664bb044afc827284b0d88f20a8808923b3cf4
PaintRock <ann.knode@atlasschool.com>
2024-02-08 20:25:45 +0000

    SornottoS

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Create SQL table with 3 attributes and a partridge
-CREATE TABLE IF NOT EXIST users(
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255),
     name VARCHAR(255),
