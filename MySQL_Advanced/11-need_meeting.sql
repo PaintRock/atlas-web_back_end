6dde1dfff48ada3c4ccd40033651fb7162e05d3f
PaintRock <ann.knode@atlasschool.com>
2024-02-15 00:25:32 +0000

    eleven2

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,9 +1,8 @@
 -- Create need_meeting 
 DELIMITER //
-CREATE VIEW need_meeting
-FROM names AS students
-WHERE score < 80 
-AND last_meeting NOT NULL
-OR last_meeting (NOW() - 1 month);
+CREATE VIEW need_meeting AS
+SELECT name, score, IFNULL(last_meeting, 1) AS ISNULL_last_meeting
+FROM students
+WHERE (score < 80 OR last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
 END;
 //
