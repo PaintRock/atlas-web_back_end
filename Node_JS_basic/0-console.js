cca210fa62aa2fa2a0e54b1160d5b82563b9df37
PaintRock <ann.knode@atlasschool.com>
2024-02-19 19:15:08 +0000

    zero5

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
-function displayMessage(string) {
-  console.log(string);
+// Comment to test file 2
+
+function displayMessage (string) {
+  console.log(string)
 }
 
-module.exports = displayMessage;
+  module.exports = displayMessage
